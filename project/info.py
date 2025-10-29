01c6909cfe28785b4bc174fd8af53f36ba22842d
krish Akbari <krishakbari5221@gmail.com>
2025-10-29 11:01:10 +0530

    Update info.py

--- project/info.py
+++ project/info.py
@@ -2,6 +2,7 @@ from django.conf.global_settings import EMAIL_USE_TLS, EMAIL_HOST, EMAIL_HOST_PA
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'll.krish.ll1310@gmail.com'
-EMAIL_HOST_PASSWORD = 'etql lljb apps lnwf'
+EMAIL_HOST_USER = 'demo@gmail.com'  # your emai id 
+EMAIL_HOST_PASSWORD = 'xxxx xxxx xxxx xxxx'  # your google app password
+
 EMAIL_PORT = 587
